ea1a855577049588973eec03341cfbee7a857c9d
Arthur <arthur.samy.93@gmail.com>
2020-02-16 14:33:23 +0100

    ''

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import app from "./app";
 import {PORT} from "./constants/news.constant";
 
-app.listen(PORT, () => {
+app.listen(process.env.PORT || PORT, () => {
+  console.log(process.env.PORT)
   console.log("Listening on port " + PORT);
 });
